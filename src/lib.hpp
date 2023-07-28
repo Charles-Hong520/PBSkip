d9d9a7bbb195a31fc9b30833b6e724feb622c949
P-Dhruv20 <dhruvparmar20@gmail.com>
2023-07-28 13:44:57 -0700

    consume address and person

--- src/lib.hpp
+++ src/lib.hpp
@@ -7,44 +7,44 @@
 #include <iostream>
 
 struct Buffer {
-  uint8_t* buffer;
-  size_t size;
+    uint8_t* buffer;
+    size_t size;
 
-  Buffer(std::string& filename) {
-    int fd = open(filename.c_str(), O_RDONLY);
-    struct stat st;
-    stat(filename.c_str(), &st);
-    this->size = st.st_size;
-    this->buffer = (uint8_t*)mmap(0, size, PROT_READ, MAP_FILE | MAP_PRIVATE, fd, 0);
-  }
+    Buffer(std::string& filename) {
+        int fd = open(filename.c_str(), O_RDONLY);
+        struct stat st;
+        stat(filename.c_str(), &st);
+        this->size = st.st_size;
+        this->buffer = (uint8_t*)mmap(0, size, PROT_READ, MAP_FILE | MAP_PRIVATE, fd, 0);
+    }
 };
 
 struct Seeker {
-  uint8_t* buffer;
-  uint32_t curr;
-  uint32_t end;
+    uint8_t* buffer;
+    uint32_t curr;
+    uint32_t end;
 
-  Seeker(const Buffer& buffer, size_t start, size_t length) {
-    if (buffer.size - length < start) {
-      std::cout << "ERROR: Seeker Constructor: end is past eof" << std::endl;
-      exit(0);
-    }
-    this->end = start + length;
-    this->curr = start;
-    this->buffer = buffer.buffer;  // always 0th index/beg of file
-  }
-  uint32_t ReadTag() {
-    uint32_t tag = 0;
-    if (curr < end) {
-      tag = buffer[curr];
-      if (tag < 0x80) {
-        curr++;
-        return tag;
-      }
+    Seeker(const Buffer& buffer, size_t start, size_t length) {
+        if (buffer.size - length < start) {
+            std::cout << "ERROR: Seeker Constructor: end is past eof" << std::endl;
+            exit(0);
+        }
+        this->end = start + length;
+        this->curr = start;
+        this->buffer = buffer.buffer;  // always 0th index/beg of file
     }
 
-    return 0;
-  }
+    uint32_t ReadTag() {
+        uint32_t tag = 0;
+        if (curr < end) {
+            tag = buffer[curr];
+            if (tag < 0x80) {
+                curr++;
+                return tag;
+            }
+        }
+        return 0;
+    }
 };
 
 #endif
--- src/sequential/person.pbs.cpp
+++ src/sequential/person.pbs.cpp
@@ -1,108 +1,121 @@
 #include "person.pbs.h"
 void Address::set_zipcode(uint32_t zipcode) {
-  this->zipcode = zipcode;
+    this->zipcode = zipcode;
 }
 void Address::set_international(bool b) {
-  this->international = b;
+    this->international = b;
 }
 void Address::set_city(const std::string& str) {
-  this->city = str;
+    this->city = str;
 }
 bool Address::parseAddress(Seeker& seek) {
-  uint32_t tag;
-
-  return true;
+    uint32_t tag = seek.ReadTag();
+    while (tag != 0) {
+        uint32_t field_id = tag >> 3;
+        uint32_t wire = tag & 7;
+        switch (field_id) {
+            case 1:  // zipcode
+                if (wire == 0) {
+                    uint64_t i;
+                    seek.ReadVarint64(&i);
+                    address.set_zipcode(i);
+                }
+                break;
+            case 2:  // international
+                if (wire == 0) {
+                    uint64_t i;
+                    seek.ReadVarint64(&i);
+                    address.set_international(i);
+                }
+                break;
+            case 3:  // city
+                if (wire == 2) {
+                    uint32_t len;
+                    seek.ReadVarint32(&len);
+                    std::string buffer;
+                    seek.ReadString(&buffer, len);
+                    address.set_city(buffer);
+                }
+                break;
+        }
+    }
+    return true;
 }
 
 void Person::set_name(const std::string& str) {
-  this->name = str;
+    this->name = str;
 }
 void Person::set_age(int64_t age) {
-  this->age = age;
+    this->age = age;
 }
 void Person::set_address(Address* addr) {
-  this->address = addr;
+    this->address = addr;
 }
 void Person::add_names(const std::string& str) {
-  this->names.push_back(str);
+    this->names.push_back(str);
 }
 void Person::add_ages(int64_t age) {
-  this->ages.push_back(age);
+    this->ages.push_back(age);
 }
 void Person::add_addresses(Address* addr) {
-  this->addresses.push_back(addr);
+    this->addresses.push_back(addr);
 }
-bool Person::parsePerson(Seeker&) {
-  return true;
+bool Person::parsePerson(Seeker& seek) {
+    uint32_t tag = seek.ReadTag();
+    while (tag != 0) {
+        uint32_t field_id = tag >> 3;
+        uint32_t wire = tag & 7;
+        switch (field_id) {
+            case 1:  // name
+                if (wire == 2) {
+                    uint32_t len;
+                    seek.ReadVarint32(&len);
+                    std::string buffer;
+                    seek.ReadString(&buffer, len);
+                    person.set_name(buffer);
+                }
+                break;
+            case 2:  // age
+                if (wire == 0) {
+                    uint64_t i;
+                    seek.ReadVarint64(&i);
+                    person.set_age(i);
+                }
+                break;
+            case 3:  // address
+                if (wire == 2) {
+                    uint32_t len;
+                    seek.ReadVarint32(&len);
+                    Address* addr;
+                    addr->parseAddress(seek);
+                    person.set_address(addr);
+                }
+                break;
+            case 4:  // repeated names
+                if (wire == 2) {
+                    uint32_t len;
+                    seek.ReadVarint32(&len);
+                    std::string buffer;
+                    seek.ReadString(&buffer, len);
+                    person.add_names(buffer);
+                }
+            case 5:  // repeated ages
+                if (wire == 0) {
+                    uint64_t i;
+                    seek.ReadVarint64(&i);
+                    person.add_ages(i);
+                }
+                break;
+            case 6:  // repeated addresses
+                if (wire == 2) {
+                    uint32_t len;
+                    seek.ReadVarint32(&len);
+                    Address* addr;
+                    addr->parseAddress(seek);
+                    person.add_addresses(addr);
+                }
+                break;
+        }
+    }
+    return true;
 }
-// bool Person::parsePerson(Seeker& seek) {
-//   uint32_t tag = input->ReadTag();
-//   // cout<<input->CurrentPosition()<<endl;
-//   if (tag == 0) {
-//     // encounter EOF or tag == 0
-//     // 1. EOF: when ReadTag() returns 0 when called exactly at EOF
-//     // 2. tag parsed as 0: a byte 0x00 is interpreted as a tag, indicating a wrong parse
-//     return false;
-//   }
-//   uint32_t field_id = tag >> 3;
-//   uint32_t wire = tag & 7;
-//   // handle a record
-//   switch (field_id) {
-//     case 1:  // name
-//       if (wire == 2) {
-//         uint32_t len;
-//         input->ReadVarint32(&len);
-//         std::string buffer;
-//         input->ReadString(&buffer, len);
-//         person.set_name(buffer);
-//       }
-//       break;
-//     case 2:  // age
-//       if (wire == 0) {
-//         uint32_t i;
-//         input->ReadVarint32(&i);
-//         person.set_age(i);
-//       }
-//       break;
-//     case 3:  // phone number
-//       if (wire == 2) {
-//         // repeat email
-//         uint32_t len;
-//         input->ReadVarint32(&len);
-//         std::string buffer;
-//         input->ReadString(&buffer, len);
-//         person.set_phone_numbers(buffer);
-//       }
-//       break;
-//     case 4:  // hobby
-//       if (wire == 2) {
-//         uint32_t len;
-//         input->ReadVarint32(&len);
-//         std::string buffer;
-//         input->ReadString(&buffer, len);
-//         person.set_hobby(buffer);
-//       }
-//       break;
-//     case 5:  // message addr
-//       if (wire == 2) {
-//         uint32_t len;
-//         input->ReadVarint32(&len);
-
-//         uint32_t currPos = input->CurrentPosition();
-//         Address* addr = person.add_addr();
-//         input->Skip(len);
-
-//         // thread pool
-
-//         // trds[i]=std::thread(&Parser::give_to_worker, this, currPos, len, addr);
-//         // pool.push_task(&Parser::give_to_worker, this, currPos, len, addr);
-
-//         // seq
-//         gp::io::CodedInputStream* copyStream = new gp::io::CodedInputStream(
-//             content + currPos, len);
-//         addr->ParseFromCodedStream(copyStream);
-//       }
-//       break;
-//   }
-//   return true;
-// }
