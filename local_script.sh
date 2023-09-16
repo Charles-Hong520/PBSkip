a13afd6c246c7b422c1e08d225781d8af61aeda6
P-Dhruv20 <dhruvparmar20@gmail.com>
2023-09-15 20:24:10 -0700

    print incorrect valid bytes

---
+++ local_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+protoc --proto_path=src/schema/ --cpp_out=src/schema/ profile.proto
+g++ -std=c++17 src/pthreads/profile.pbs.cpp src/schema/profile.pb.cc src/pthreads/testprofile.cpp -lprotobuf -lpthread -O3 -o src/pthreads/testprofile
+g++ -std=c++17 src/generated/profile.pbs.cpp src/schema/profile.pb.cc src/sequential/testprofile.cpp -lprotobuf -lpthread -O3 -o src/sequential/testprofile
+g++ -std=c++17 src/parallel/profile.pbs.cpp src/schema/profile.pb.cc src/parallel/testprofile.cpp -lprotobuf -lpthread -O3 -o src/parallel/testprofile
+g++ -std=c++17 src/schema/profile.pb.cc src/google_api/testprofile.cpp -lprotobuf -lpthread -O3 -o src/google_api/testprofile
+export PBS_NUM_THREADS=2
+echo "------------------Running the 2 pthreads version-------------------"
+for i in {1..5}; do src/pthreads/testprofile; done
+export PBS_NUM_THREADS=4
+echo "------------------Running the 4 pthreads version-------------------"
+for i in {1..5}; do src/pthreads/testprofile; done
+export PBS_NUM_THREADS=8
+echo "------------------Running the 8 pthreads version-------------------"
+for i in {1..5}; do src/pthreads/testprofile; done
+export PBS_NUM_THREADS=16
+echo "------------------Running the 16 pthreads version------------------"
+for i in {1..5}; do src/pthreads/testprofile; done
+export PBS_NUM_THREADS=32
+echo "------------------Running the 32 pthreads version------------------"
+for i in {1..5}; do src/pthreads/testprofile; done
+export PBS_NUM_THREADS=64
+echo "------------------Running the 64 pthreads version------------------"
+for i in {1..5}; do src/pthreads/testprofile; done
+echo "------------------Running the Sequential verison-------------------"
+for i in {1..5}; do src/sequential/testprofile; done
+echo "------------------Running the Parallel verison---------------------"
+for i in {1..5}; do src/parallel/testprofile; done
+echo "------------------Running the Google API verison-------------------"
+for i in {1..5}; do src/google_api/testprofile; done
--- src/dac/time.cpp
+++ src/dac/time.cpp
@@ -23,57 +23,59 @@
 g++ -std=c++17 src/dac/profile.pbs.cpp src/schema/profile.pb.cc src/dac/time.cpp -lprotobuf -lpthread -O3 -o src/dac/testprofile && src/dac/testprofile
 */
 int main() {
-  int PBS_NUM_THREADS = 2;
-  if (const auto env_p = std::getenv("PBS_NUM_THREADS")) {
-    PBS_NUM_THREADS = std::stoi(env_p);
-  }
-  std::cout << "threads: " << PBS_NUM_THREADS << " \n";
-  // std::cout << "---------------------Running the parallel version----------------------\n";
-  std::string file = "dataset/zz.prof";
-  Buffer content(file);
-
-  std::chrono::time_point<std::chrono::system_clock> start_T, end_T;
-  std::chrono::duration<double> elapsed_seconds;
-  //-----------------------------------
-
-  // parses our PBSkip object
-
-  // std::cout << "Parsing " << file << " with custom pthreads" << std::endl;
-  // std::cout << "content size: " << content.size << std::endl;
-
-  Seeker seeker(content, 0, content.size);
-
-  PBS::Profile* pbs = new PBS::Profile();
-  pbs->fillTracker(seeker);
-  auto tr = pbs->getTracker();
-  std::map<int, int> pos_to_fi;
-
-  int z = 0;
-  for (const auto& [field_id, vpos] : tr) {
-    std::cout << "field_id: " << field_id << ", size: " << vpos.size() << std::endl;
-    for (auto& [pos, len] : vpos) {
-      pos_to_fi[pos] = len;
-      if (pos >= 1078689392 - 100 && pos <= 1078689392 + 400) {
-        print("field_id", field_id, "pos:", pos, "len:", len);
-      }
-      z++;
+    int PBS_NUM_THREADS = 2;
+    if (const auto env_p = std::getenv("PBS_NUM_THREADS")) {
+        PBS_NUM_THREADS = std::stoi(env_p);
     }
-  }
-  // 1078689392 299
-  const int startCheck = 1078689392 - 10;
-  const int endCheck = 1078689392 + 300;
-  start_T = std::chrono::system_clock::now();
-  for (int i = startCheck; i < endCheck; i++) {
-    seeker.curr = i;
-    seeker.end = content.size;
-    if (pbs->checkValidStart(seeker)) {
-      print("valid at", i);
-    }
-  }
-  end_T = std::chrono::system_clock::now();
+    std::cout << "threads: " << PBS_NUM_THREADS << " \n";
+    // std::cout << "---------------------Running the parallel version----------------------\n";
+    std::string file = "dataset/zz.prof";
+    Buffer content(file);
 
-  seeker.curr = 0;
-  // pbs->parseProfile(seeker);
-  elapsed_seconds = end_T - start_T;
-  std::cout << "time: " << elapsed_seconds.count() << "s\n";
+    std::chrono::time_point<std::chrono::system_clock> start_T, end_T;
+    std::chrono::duration<double> elapsed_seconds;
+    //-----------------------------------
+
+    // parses our PBSkip object
+
+    // std::cout << "Parsing " << file << " with custom pthreads" << std::endl;
+    // std::cout << "content size: " << content.size << std::endl;
+
+    Seeker seeker(content, 0, content.size);
+
+    PBS::Profile* pbs = new PBS::Profile();
+    pbs->fillTracker(seeker);
+    auto tr = pbs->getTracker();
+    std::map<uint64_t, uint64_t> pos_to_fi;
+
+    int z = 0;
+    for (const auto& [field_id, vpos] : tr) {
+        std::cout << "field_id: " << field_id << ", size: " << vpos.size() << std::endl;
+        for (auto& [pos, len] : vpos) {
+            pos_to_fi[pos] = len;
+            if (pos >= 1078689392 - 100 && pos <= 1078689392 + 400) {
+                print("field_id", field_id, "pos:", pos, "len:", len);
+            }
+            z++;
+        }
+    }
+    // 1078689392 299
+    const int startCheck = 1078689392 - 10;
+    const int endCheck = 1078689392 + 300;
+    start_T = std::chrono::system_clock::now();
+    for (int i = startCheck; i < endCheck; i++) {
+        seeker.curr = i;
+        seeker.end = content.size;
+        if (pbs->checkValidStart(seeker)) {
+            uint64_t temp;
+            seeker.ReadVarint64(&temp);
+            print("valid at", i, "value:", temp);
+        }
+    }
+    end_T = std::chrono::system_clock::now();
+
+    seeker.curr = 0;
+    // pbs->parseProfile(seeker);
+    elapsed_seconds = end_T - start_T;
+    std::cout << "time: " << elapsed_seconds.count() << "s\n";
 }
